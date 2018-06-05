a06352cd179352474f06b26be120bcd247f6a521
David <drmarq01@gmail.com>
2018-06-05 19:24:15 -0400

    Added flex coding and media query NEED TO FIX FOOTERgit statusgit status

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 window.alert("VELCOME TO ZEE VILD & VOOLLY VEBPAGE");
+
+//********************Funny field input swap 'button'*************
+//const bttn = document.querySelector('label.bttn');
+
+//bttn.addEventListener('click', () => {
+//  bttn.textContent="Are You Sure";
+// });
