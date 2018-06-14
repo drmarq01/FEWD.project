5bd809affbf58e8429efe8d893b33225d9f769ad
David <drmarq01@gmail.com>
2018-06-14 13:35:39 -0400

    added jquery and flex layout

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-window.alert("VELCOME TO ZEE VILD & VOOLLY VEBPAGE");
-
-//********************Funny field input swap 'button'*************
-//const bttn = document.querySelector('label.bttn');
-
-//bttn.addEventListener('click', () => {
-//  bttn.textContent="Are You Sure";
-// });
+$('.html').on('click', function(){
+  $('.html').css('backgroundColor', "red");
+});
