4eb358004c332a90b087aab5743357a60a02f0bc
David <drmarq01@gmail.com>
2018-07-10 19:18:11 -0400

    deleting

--- app.js
+++
@@ -1,8 +0,0 @@
-$('.button').on('click', function(event){
-  alert('You sure??');
-});
-
-$('.html').click(function() {
-   var randomColorChange = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    $(this).css('background-color', randomColorChange);
-    });
