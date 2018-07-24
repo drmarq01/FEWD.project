8daf920f6194295527bad2b1065cfde11a765bee
David <drmarq01@gmail.com>
2018-07-24 18:31:17 -0400

    edited notes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
+//                  EMAIL ALERT
+
+
+
 $('.button').on('click', function(event){
   alert('You sure??');
 });
+
+
+//         RANDOM BACKGROUND COLOR
+
 
 $('.html').click(function() {
    var randomColorChange = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
